a48d0054894f42cb22ace7679baed044bfbbf888
luyuanx2 <694436921@qq.com>
2017-12-25 17:16:24 +0800

    提交

--- src/views/personal/index.vue
+++ src/views/personal/index.vue
@@ -19,14 +19,18 @@
         </router-link>
       </div>
       <div class="personal-grid_item">
-        <i class="iconfont icon-bank-card"></i>
-        <span class="label">待还款</span>
-        <badge :text="personal.countRepayment" v-if="personal.countRepayment > 0"></badge>
+        <router-link to="/orderList/repayment">
+          <i class="iconfont icon-bank-card"></i>
+          <span class="label">待还款</span>
+          <badge :text="personal.countRepayment" v-if="personal.countRepayment > 0"></badge>
+        </router-link>
       </div>
       <div class="personal-grid_item">
-        <i class="iconfont icon-over"></i>
-        <span class="label">已逾期</span>
-        <badge :text="personal.countLoanOverdue" v-if="personal.countLoanOverdue > 0"></badge>
+        <router-link to="/orderList/overdue">
+          <i class="iconfont icon-over"></i>
+          <span class="label">已逾期</span>
+          <badge :text="personal.countLoanOverdue" v-if="personal.countLoanOverdue > 0"></badge>
+        </router-link>
       </div>
       <div class="personal-grid_item personal-grid_border">
         <i class="iconfont icon-loan"></i>
