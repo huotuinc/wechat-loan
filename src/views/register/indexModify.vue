2e259d5afa4da6da9101b31f9479e63012b22b2c
sjw0608 <3486106508@qq.com>
2018-03-23 18:00:16 +0800

    修改部分登录界面和注册界面

---
+++ src/views/register/indexModify.vue
@@ -0,0 +1,14 @@
+<template>
+  <form-register></form-register>
+</template>
+
+<script>
+import formRegister from '@/components/formModify/formRF'
+
+export default {
+  name: 'Register',
+  components: {
+    'form-register': formRegister
+  }
+}
+</script>
